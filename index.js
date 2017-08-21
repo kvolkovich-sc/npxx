035c024403b1e73db69c08171866e594507fee35
Kirill Volkovich <volkovich@scand.com>
2017-08-21 18:05:33 +0300

    Updates

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 const path = require('path');
 const fs = require('fs');
 
-const SCRIPTS_DIR = path.resolve(__dirname, '.npxx');
-const SCRIPTS_DIR_FILES = fs.readdirSync(SCRIPTS_DIR, { encoding: 'utf-8' });
+let SCRIPTS_DIR = path.resolve(__dirname, '.npxx');
+let SCRIPTS_DIR_FILES = fs
+    .readdirSync(SCRIPTS_DIR, { encoding: 'utf-8' })
+    .filter(file => !/^\..*/.test(file)); // restrict hidden files
 
 console.log(SCRIPTS_DIR_FILES);
